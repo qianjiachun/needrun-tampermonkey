3ec7a9359620da7cf2f7f4ac7abe73fd2658cf15
qianjiachun <189964430@qq.com>
2023-04-15 19:57:26 +0800

    fix: 修复igxe无法跳转的bug

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@
 // @description  得润(need.run)官方脚本，用于buff、悠悠有品、igxe、c5饰品直达自由交流区，无惧敏感词
 // @icon         https://s1.imagehub.cc/images/2023/04/12/d3461ee6e5a2ffa11b0903bf46b6b862.png
 // @namespace    https://github.com/qianjiachun
-// @version      2023.04.12.01
+// @version      2023.04.15.01
 // @author       小淳
 // @match        *://buff.163.com/goods*
 // @match        *://www.youpin898.com/goodInfo*
--- src/pages/igxe/packages/ChatButton/index.js
+++ src/pages/igxe/packages/ChatButton/index.js
@@ -1,4 +1,6 @@
 import {
+  getNeedRunUrlByGoodsInfo,
+  openPage,
   renderChatButton,
   showMessage,
 } from "../../../../utils";
